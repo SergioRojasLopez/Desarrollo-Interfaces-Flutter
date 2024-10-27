2a18a91e02f647843aa678277aa2d0065cd6a8d5
SergioRojasLopez <115539369+SergioRojasLopez@users.noreply.github.com>
2024-10-27 15:09:15 +0100

    -- ejercicio3.dart finished

--- lib/ejercicio3.dart
+++ lib/ejercicio3.dart
@@ -6,88 +6,174 @@ class Ejercicio3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.red,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          mainAxisSize: MainAxisSize.min,
-          verticalDirection: VerticalDirection.down,
-          children: [
-            Container(
-              width: 100.0,
-              height: 100.0 ,
-              color: Colors.white,
-              child: Center(
-                child: point(true),
-              ),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              // alignment: Alignment.bottomRight,
-              color: Colors.green,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
+    return Scaffold(
+        backgroundColor: Colors.green,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
                 children: [
+                  // Dado 1
                   Container(
-                      height: 10,
-                      width: 10,
-                      color: Colors.black,
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey[100],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        point(),
+                      ],
+                    ),
                   ),
+                  // Dado 2
                   Container(
-                    height: 10,
-                    width: 10,
-                    color: Colors.black,
-                  )
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey[200],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          verticalDirection: VerticalDirection.up,
+                          children: [
+                            point(),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            point(),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  // Dado 3
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey[300],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          verticalDirection: VerticalDirection.up,
+                          children: [
+                            point(),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            point(),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            point(),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  // Dado 4
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey[400],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            point(),
+                            point()
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            point(),
+                            point()
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  // Dado 5
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey[500],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            point(),
+                            point()
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            point()
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            point(),
+                            point()
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  // Dado 6
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.grey[600],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            point(),
+                            point(),
+                            point()
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            point(),
+                            point(),
+                            point()
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              // alignment: Alignment.bottomLeft,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              // alignment: Alignment.topRight,
-              color: Colors.white,
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              // alignment: Alignment.topRight,
-              color: Colors.black,
-            ),
-            Container(
-              width: 100.0,
-              height: 100.0,
-              // alignment: Alignment.topRight,
-              color: Colors.yellow,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    child: point(true),
-                  )
-                ],
-              ),
-            )
-          ],
 
-        ),
-      ),
+            ],
+          ),
+        )
     );
   }
-  Widget point ([bool margin = false]){
+
+  Widget point() {
     return Container(
-      height: 10,
+      margin: const EdgeInsets.all(10),
       width: 10,
+      height: 10,
       color: Colors.black,
-      margin: margin ? const EdgeInsets.all(5) : EdgeInsets.zero,
     );
   }
 
